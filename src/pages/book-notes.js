c24323ec841921150525763d00e2466735fef5fa
Luke MacKenzie <mackenzieluke94@gmail.com>
2020-07-23 22:13:13 -0400

    added more notes

--- src/pages/book-notes.js
+++ src/pages/book-notes.js
@@ -13,6 +13,14 @@ export const query = graphql`
           author
           note1
           note2
+          note3
+          note4
+          note5
+          note6
+          note7
+          note8
+          note9
+          note10
         }
       }
     }
