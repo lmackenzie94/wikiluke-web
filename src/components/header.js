83c50e1fa389512062dd8b376387815cf2cbb1bf
Luke MacKenzie <mackenzieluke94@gmail.com>
2020-07-27 12:26:15 -0400

    minor fix

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import styled from "styled-components"
-import { Wrapper } from "./styles/globalStyles"
+import { Wrapper } from "./styles/GlobalStyles"
 
 const HeaderStyles = styled.header`
   background: var(--lightGray);
--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
 import "normalize.css"
-import GlobalStyles, { Wrapper } from "./styles/globalStyles"
+import GlobalStyles, { Wrapper } from "./styles/GlobalStyles"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
