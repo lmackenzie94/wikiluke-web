7c9874f81a72e4286200256d997a6db8f854d693
Luke MacKenzie <mackenzieluke94@gmail.com>
2020-08-02 22:19:15 -0400

    added context provider to ssr file (need to figure out how to actually solve the build fail issue)

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,13 +2,7 @@ import React from "react"
 import { ThemeColourProvider } from "./src/contexts/themeColourContext"
 
 export const wrapRootElement = ({ element }) => {
-  const defaultColour = localStorage
-    ? localStorage.getItem("themeColour") || "#008b02"
-    : "#008b02"
-
   return (
-    <ThemeColourProvider defaultColour={defaultColour}>
-      {element}
-    </ThemeColourProvider>
+    <ThemeColourProvider defaultColour="#008b02">{element}</ThemeColourProvider>
   )
 }
